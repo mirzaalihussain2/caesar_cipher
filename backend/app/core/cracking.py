129b6fc9483a595035480bd51612479ab29ebe31
mirzaalihussain2 <mirzaalihussain2@gmail.com>
2025-01-30 17:54:39 +0000

    Removing hacking file entirely, replaced by cracking.py. Tests all pass.

--- backend/app/core/cracking.py
+++ backend/app/core/cracking.py
@@ -1,8 +1,8 @@
-from .utils import count_alpha_characters, unigram_frequencies, bigram_frequencies, MIN_BIGRAM_TEXT_LENGTH, ConfidenceLevel, ANALYSIS_LENGTHS
-from .hacking import generate_base_solutions, add_solution_statistics
-from .types import StatName, HackResult, SolutionWithTotal
-from .chi_squared import calculate_chi_squared_total
+from .utils import count_alpha_characters, unigram_frequencies, bigram_frequencies, MIN_BIGRAM_TEXT_LENGTH, ConfidenceLevel, ANALYSIS_LENGTHS, DECIMAL_PLACES
+from .types import Solution, StatName, HackResult, SolutionWithTotal
+from .chi_squared import calculate_chi_squared_total, run_chi_squared_test, normalise_chi_squared_stats
 from .confidence import set_confidence
+from .encryption import encrypt_text
 
 def crack_cipher(ciphertext: str) -> HackResult:
     """
@@ -72,3 +72,35 @@ def get_analysis_length(full_text_length: int, analysis_lengths: list[int]) -> i
     else:
         next_length = sorted(analysis_lengths)[0]
         return min(full_text_length, next_length)
+
+def add_solution_statistics(
+        solutions: list[Solution],
+        ngram_size: int,
+        ngram_expected_frequencies: dict[StatName, float],
+        stat_name: StatName
+) -> list[Solution]:
+    for solution in solutions:
+        chi_squared_stat = run_chi_squared_test(
+            text=solution['text'],
+            ngram_size=ngram_size,
+            normalised_expected_frequencies=ngram_expected_frequencies
+        )
+        solution['chi_squared_stats'][stat_name] = round(chi_squared_stat, DECIMAL_PLACES)
+    
+    normalised_solutions = normalise_chi_squared_stats(solutions, stat_name)
+    return normalised_solutions
+
+def generate_base_solutions(ciphertext: str, analysis_length: int) -> list[Solution]:
+    solutions = []
+
+    for key in range(1, 26):
+        solution = {
+            'key': key,
+            'full_text': encrypt_text(ciphertext, key),
+            'text': encrypt_text(ciphertext[:analysis_length], key),
+            'chi_squared_stats': {},
+            'normalised_chi_squared_stats': {}
+        }
+        solutions.append(solution)
+
+    return solutions
