a2d2eff66d6749261e0ed44b8668e9f658412a3b
mirzaalihussain2 <mirzaalihussain2@gmail.com>
2025-01-31 12:10:00 +0000

    Adding detail on confidence stats, why & how they were chosen.

--- backend/app/hacking/confidence.py
+++ backend/app/hacking/confidence.py
@@ -17,15 +17,33 @@ def calculate_confidence_stats(chi_squared_values: list[float]) -> ConfidenceThr
     mean = statistics.mean(sorted_values[1:])
     std_dev = statistics.stdev(sorted_values[1:])
 
+    # Z-statistic measures the difference between the best match (aka lowest value) and the mean
+    # Commonly used in statistics
+    z_statistic = (mean - lowest_value) / std_dev
+    
+    # Separation score is similar to Z-statistic, but it's adapted to frequency analysis,
+    # where the chi-squared values for incorrect decryption keys will not be normally distributed
+    # and one chi-squared value should stand very distinct from the rest.
+    # Separation score measures the difference between the best match and the second best match.
+    separation_score = (second_lowest_value - lowest_value) / std_dev
+    
+    # Relative rank ratio normalises the best match against the mean.
+    # A much lower chi-squared value than the mean suggests a standout match.
+    # Simpler to interpret than other two statistical measures. Inspired by signal-to-noise ratio in statistics.
+    relative_rank = lowest_value / mean
+
     return ConfidenceThreshold(
-        z_statistic=(mean - lowest_value) / std_dev,
-        separation_score=(second_lowest_value - lowest_value) / std_dev,
-        relative_rank=lowest_value / mean
+        z_statistic=z_statistic,
+        separation_score=separation_score,
+        relative_rank=relative_rank
     )
 
 def get_confidence_level(stats: ConfidenceThreshold) -> ConfidenceLevel:
     low_threshold = CONFIDENCE_THRESHOLDS[ConfidenceLevel.LOW]
     high_threshold = CONFIDENCE_THRESHOLDS[ConfidenceLevel.HIGH]
+
+    # Confidence Thresholds based on best practice thresholds (e.g. Z-statistic is a common statistical measure).
+    # We computed chi-squared values on test ciphertexts and examined the confidence stats to calibrate these thresholds.
 
     if (
         stats.separation_score < low_threshold.separation_score or 
