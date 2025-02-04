cab43a23a05c5511ba0afcee72f6bee10bb864f1
mirzaalihussain2 <mirzaalihussain2@gmail.com>
2025-02-04 17:59:36 +0000

    Basic form working

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,23 +1,78 @@
 "use client"
 
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Switch } from "@/components/ui/switch";
 import { Textarea } from "@/components/ui/textarea";
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
- 
-import { toast } from "@/components/hooks/use-toast"
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+ 
+import { toast } from "@/components/hooks/use-toast";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+const FormSchema = z.object({
+  text: z.string().max(10000, {
+    message: "Input message cannot exceed 10,000 characters"
+  }),
+  key: z.number().int(),
+  keep_spaces: z.boolean(),
+  keep_punctuation: z.boolean(),
+  transform_case: z.enum(["keep_case", "lowercase", "uppercase"])
+})
 
 export default function Home() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema)
+  })
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(JSON.stringify(data, null, 2))
+    return toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    })
+  }
+
   return (
     <main className="container mx-auto px-4 max-w-3xl">
-      <div>
-        <Textarea></Textarea>
-      </div>
-
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+          <FormField
+            control={form.control}
+            name="text"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Text</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Tell us a little bit about yourself"
+                    className="resize-none"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>
+                  You can <span>@mention</span> other users and organisations.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </main>
   );
 }
